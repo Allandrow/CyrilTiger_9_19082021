7e128c104a3a25afef5215c34676009aa93fc410
Cyril Tiger <cyriltiger@gmail.com>
2021-08-30 11:46:42 +0200

    added coverage to newBill.js handleChangeFile function #5

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,13 +1,39 @@
-// import { screen } from '@testing-library/dom'
+import { screen } from '@testing-library/dom'
 import NewBillUI from '../views/NewBillUI.js'
-// import NewBill from '../containers/NewBill.js'
+import NewBill from '../containers/NewBill.js'
+import { ROUTES } from '../constants/routes'
+import { localStorageMock } from '../__mocks__/localStorage.js'
+import userEvent from '@testing-library/user-event'
 
 describe('Given I am connected as an employee', () => {
   describe('When I am on NewBill Page', () => {
-    test('Then ...', () => {
+    test('Then i can attach a jpg/jpeg/png file to the form via an input', () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      // to-do write assertion
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem(
+        'user',
+        JSON.stringify({
+          type: 'Employee'
+        })
+      )
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore: null,
+        localStorage: window.localStorage
+      })
+
+      const file = new File(['test'], 'test.png', { type: 'image/png' })
+      const input = screen.getByTestId('file')
+      const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e))
+      input.addEventListener('change', handleChangeFile)
+      userEvent.upload(input, file)
+      expect(handleChangeFile).toHaveBeenCalled()
     })
   })
 })
