a7ffe1ea639e58c1fb6ec1aebd4b536bd7179acd
Cyril Tiger <cyriltiger@gmail.com>
2021-09-01 17:03:52 +0200

    reworked test for handleSubmit

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,4 +1,4 @@
-import { fireEvent, screen } from '@testing-library/dom'
+import { screen } from '@testing-library/dom'
 import '@testing-library/jest-dom'
 import NewBillUI from '../views/NewBillUI.js'
 import NewBill from '../containers/NewBill.js'
@@ -40,48 +40,62 @@ describe('Given I am connected as an employee', () => {
   })
 
   describe('When I am on NewBill Page', () => {
-    test('Then i can attach an image to the form via an input', () => {
-      const file = new File(['test'], 'test.png', { type: 'image/png' })
-      const spy = jest.spyOn(newBill, 'handleChangeFile')
-      const input = screen.getByTestId('file')
-      userEvent.upload(input, file)
-      expect(spy).toHaveBeenCalled()
-      expect(input.files[0]).toStrictEqual(file)
-      expect(input.files).toHaveLength(1)
+    describe('When I attach a file to the form', () => {
+      test('if it is an image it should be in the file handler', () => {
+        const file = new File(['test'], 'test.png', { type: 'image/png' })
+        const spy = jest.spyOn(newBill, 'handleChangeFile')
+        const input = screen.getByTestId('file')
+        userEvent.upload(input, file)
+        expect(spy).toHaveBeenCalled()
+        expect(input.files[0]).toStrictEqual(file)
+        expect(input.files).toHaveLength(1)
+      })
+      test('if it is not an image it should display an error message', () => {
+        const file = new File(['wrongTest'], 'wrongfile.pdf', { type: 'application/pdf' })
+        const spy = jest.spyOn(newBill, 'handleChangeFile')
+        const input = screen.getByTestId('file')
+        userEvent.upload(input, file)
+        expect(spy).toHaveBeenCalled()
+        expect(input.value).toBe('')
+        expect(
+          screen.getByText(/Votre justificatif doit être de type jpg, jpeg ou png/)
+        ).toBeVisible()
+      })
     })
-    test('Then i cannot attach a file other than an image to the form', () => {
-      const file = new File(['wrongTest'], 'wrongfile.pdf', { type: 'application/pdf' })
-      const spy = jest.spyOn(newBill, 'handleChangeFile')
-      const input = screen.getByTestId('file')
-      userEvent.upload(input, file)
-      expect(spy).toHaveBeenCalled()
-      expect(input.value).toBe('')
-      expect(
-        screen.getByText(/Votre justificatif doit être de type jpg, jpeg ou png/)
-      ).toBeVisible()
+    describe('When I submit a correctly filled form', () => {
+      test('Then it should create a new bill', () => {
+        const email = JSON.parse(localStorage.getItem('user')).email
+        newBill.createBill = (bill) => bill
+        const validBill = {
+          email,
+          type: 'Transports',
+          name: 'test',
+          amount: 500,
+          date: '2021-08-31',
+          vat: '70',
+          pct: 20,
+          commentary: '',
+          fileUrl: 'https://test.com/test.png',
+          fileName: 'test.png',
+          status: 'pending'
+        }
+
+        screen.getByTestId('expense-type').value = validBill.type
+        screen.getByTestId('expense-name').value = validBill.name
+        screen.getByTestId('datepicker').value = validBill.date
+        screen.getByTestId('amount').value = `${validBill.amount}`
+        screen.getByTestId('vat').value = `${validBill.vat}`
+        screen.getByTestId('pct').value = `${validBill.pct}`
+        newBill.fileName = 'test.png'
+        newBill.fileUrl = 'https://test.com/test.png'
+
+        const btn = screen.getByRole('button')
+        const spy = jest.spyOn(newBill, 'handleSubmit')
+        const spyCreateBill = jest.spyOn(newBill, 'createBill')
+        userEvent.click(btn)
+        expect(spy).toHaveBeenCalled()
+        expect(spyCreateBill).toHaveBeenCalledWith(validBill)
+      })
     })
   })
-
-  // describe('When I am on NewBill Page', () => {
-  //   test('Then I can submit a correctly filled form', () => {
-  //     // TODO : reassess what this test should do
-  //     // required inputs : select (expense-type), date (datepicker), amount (amount), pct (pct), file (file)
-  //     const select = screen.getByTestId('expense-type')
-  //     const dateInput = screen.getByTestId('datepicker')
-  //     const amountInput = screen.getByTestId('amount')
-  //     const pctInput = screen.getByTestId('pct')
-  //     const fileInput = screen.getByTestId('file')
-  //     const file = new File(['test'], 'test.png', { type: 'image/png' })
-  //     userEvent.upload(fileInput, file)
-  //     select.selectedIndex = 1
-  //     // dateInput.value = '2021-08-31'
-  //     // amountInput.value = '70'
-  //     // pctInput.value = '20'
-
-  //     const spy = jest.spyOn(newBill, 'handleSubmit')
-  //     const form = screen.getByRole('form')
-  //     fireEvent.submit(form)
-  //     expect(spy).toHaveBeenCalled()
-  //   })
-  // })
 })
